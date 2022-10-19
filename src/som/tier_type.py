988bedde6a35685b85fe34ff8903c959cbd56e20
Yusuke Izawa <me@yizawa.com>
2022-10-20 03:41:21 +0900

    added type_type.py

---
+++ src/som/tier_type.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+from rlib import jit
+
+_interp_type_str = os.getenv("SOM_TIER", None)
+
+_TC = 1
+_BC = 2
+_UNKNOWN = 3
+
+
+def _get_tier_type():
+    if _interp_type_str == "1":
+        return _TC
+    if _interp_type_str == "2":
+        return _BC
+    return _UNKNOWN
+
+
+_INTERP_TYPE = _get_tier_type()
+
+
+@jit.elidable
+def is_tier1():
+    return _INTERP_TYPE == _TC or _INTERP_TYPE == _UNKNOWN
+
+
+@jit.elidable
+def is_tier2():
+    return _INTERP_TYPE == _BC
