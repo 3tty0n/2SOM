b566ed3c9779cedd4e08670fc409da66368e9a73
Yusuke Izawa <me@yizawa.com>
2022-11-21 03:01:57 +0900

    Move scripts to PySOM-plot repository

--- scripts/pysom_benchmarks.py
+++
@@ -1,4 +0,0 @@
-MACRO = ["NBody", "GraphSearch", "PageRank"]
-MICRO = ["Bounce", "Fannkuch", "Permute", "Queens", "List", "Storage", "Sieve",
-         "BubbleSort", "QuickSort", "TreeSort", "Mandelbrot"]
-TINY = ["Fibonacci", "Dispatch", "Loop", "Recurse", "Sum"]
