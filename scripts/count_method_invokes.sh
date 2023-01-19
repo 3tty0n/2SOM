1237b4a3f22a573456d885710509315e2d81636c
Yusuke Izawa <me@yizawa.com>
2023-01-19 14:33:31 +0900

    Fix count_method_invokes.sh

--- scripts/count_method_invokes.sh
+++ scripts/count_method_invokes.sh
@@ -1,17 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 benchs=(Bounce BubbleSort Fannkuch List Mandelbrot PageRank Queens QuickSort Sieve Storage Towers TreeSort
         GraphSearch NBody Json Richards DeltaBlue)
 
-[ ! -d data-method-counts ] && mkdir data-method-counts
+benchs=(DeltaBlue)
 
-benchs=(Fibonacci Loop Sum Dispatch Recurse)
+[ ! -d data-method-counts ] && mkdir data-method-counts
 
 for bm in "${benchs[@]}"; do
     printf "${bm} ..."
     innerIter=
     if [ $bm = PageRank ]; then
         innerIter=2
+    fi
+    if [ $bm = DeltaBlue ]; then
+        innerIter=10
     fi
     SOM_INTERP=BC ./som.sh \
         -cp Smalltalk:Examples/Benchmarks/LanguageFeatures:Examples/Benchmarks/NBody:Examples/Benchmarks/Json:Examples/Benchmarks/Richards:Examples/Benchmarks/GraphSearch:Examples/Benchmarks/DeltaBlue \
