fc049825616771fa996442a04d52f5c0a61e2fef
Yusuke Izawa <me@yizawa.com>
2024-03-02 15:02:17 +0900

    Update runme.py

--- scripts/runme.py
+++ scripts/runme.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import subprocess
 
 INVOCATIONS = 10
@@ -26,7 +27,8 @@ BENCHS = {
 
 NICE = ["nice", "-n-20"]
 
-BINS = ["./som-bc-jit-tier1", "./som-bc-jit-tier2", "./som-bc-jit-interp-tier1"]
+BINS = ["./som-bc-jit-tier1", "./som-bc-jit-tier2", "./som-bc-interp-tier1"]
+# BINS = ["./som-bc-jit-tier2", "./som-bc-interp-tier1"]
 
 ARGS = [
     "-cp",
@@ -34,6 +36,11 @@ ARGS = [
     "Examples/Benchmarks/BenchmarkHarness.som",
     "--gc",
 ]
+
+def mkdir(path):
+    if os.path.exists(path):
+        return
+    os.mkdir(path)
 
 
 def jit_threshold(threshold):
@@ -46,9 +53,23 @@ def jit_threshold(threshold):
 
 
 def measure_rss():
-    def gnu_time(bm, inv):
-        gnu_time = ["/usr/bin/time", "-f", "'RSS:%M KB'", "-o", "./logs/%s_%d.txt" % (bm.lower(), inv)]
+    def gnu_time(bm, inv, bin_name, output_dir):
+        gnu_time = ["/usr/bin/time", "-f", "'RSS:%M KB'", "-o", "%s/%s_%s_%d.txt" % (
+            output_dir, bm.lower(), bin_name, inv)]
         return gnu_time
+
+    def parse_bin(bin_name):
+        if bin_name == "./som-bc-jit-tier1":
+            return "threaded"
+        elif bin_name == "./som-bc-jit-tier2":
+            return "tracing"
+        elif bin_name == "./somb-bc-interp-tier1":
+            return "interp"
+        else:
+            raise Exception
+
+    output_dir = "logs-rss"
+    mkdir(output_dir)
 
     for binary in BINS:
         for bm in BENCHS:
@@ -56,7 +77,7 @@ def measure_rss():
                 extra_args, threshold = BENCHS[bm]
                 command = (
                     NICE +
-                    gnu_time(bm, inv)
+                    gnu_time(bm, inv, parse_bin(binary), output_dir)
                     + [binary]
                     + jit_threshold(threshold)
                     + ARGS
@@ -66,22 +87,29 @@ def measure_rss():
 
 
 def measure_gc_time():
+
+    output_dir = "logs-gc"
+    mkdir(output_dir)
+
     for binary in BINS:
         for bm in BENCHS:
             for inv in range(INVOCATIONS):
                 extra_args, threshold = BENCHS[bm]
+                output_file = "%s/%s_%s_%d" % (output_dir, bm.lower(), binary, inv)
                 command = (
-                    NICE +
-                    [binary, "--gc-stats"]
+                    NICE
+                    + [binary]
                     + jit_threshold(threshold)
+                    + ["--gc-stats"]
                     + ARGS
                     + [bm, "100", str(extra_args)]
                 )
-                subprocess.run(command)
+                subprocess.run(command, stdout=output_file)
 
 
 def main():
-    measure_rss()
+    # measure_rss()
+    measure_gc_time()
 
 
 if __name__ == "__main__":
