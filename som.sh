81c66c4d3eba1fced996df66cf97335c06d3a574
Yusuke Izawa <me@yizawa.com>
2024-03-02 15:16:11 +0900

    Fix som.sh

--- som.sh
+++ som.sh
@@ -7,4 +7,5 @@ if [ -z "$PYPY_DIR" ]; then
   PYPY_DIR=$DIR/pypy
 fi
 export PYTHONPATH=$DIR/src:$PYPY_DIR:$PYTHONPATH
-exec $PYTHON $DIR/src/main.py "$@"
+export SOM_INTERP=BC
+exec $PYTHON $DIR/src/main.py -cp Smalltalk:Examples/Benchmarks/Richards:Examples/Benchmarks/CD:Examples/Benchmarks/NBody:Examples/Benchmarks/DeltaBlue:Examples/Benchmarks/Json:Examples/Benchmarks/GraphSearch "$@"
